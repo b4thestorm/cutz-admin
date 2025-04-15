d7e6e812d744483204255bdb9e0f297fb6820e4c
arnoldsanders <arnoldsander@gmail.com>
2025-04-15 17:35:35 -0400

    pull profile on setup and update profile

--- src/app/components/profileForm.tsx
+++ src/app/components/profileForm.tsx
@@ -1,5 +1,5 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, TextField } from '@mui/material';
-import { SetStateAction, Dispatch, useState } from 'react';
+import { SetStateAction, Dispatch } from 'react';
 
 
 export interface ProfileFormProps {
@@ -11,12 +11,27 @@ export interface ProfileFormProps {
       title: string,
       description: string
     }
-    setProfile: Dispatch<SetStateAction<{ first_name: string; last_name: string; title: string; description: string; }>>;
+    setProfile: Dispatch<SetStateAction<{ first_name: string; last_name: string; title: string; description: string;}>>;
   }
 
 export function ProfileFormDialog(props: ProfileFormProps) {
     const { visible, profile, setProfile, setVisible } = props;
-    const [errors, setErrors] = useState(null)
+    let csrftoken: string;
+
+    const getCookie = (name: string) => {
+      let cookieValue = null;
+      if (document.cookie && document.cookie !== '') {
+          const cookies = document.cookie.split(';');
+          for (let i = 0; i < cookies.length; i++) {
+              const cookie = cookies[i].trim();
+              if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+  }
   
     const handleChange = (event: React.SyntheticEvent<EventTarget>) => {
         const element = event.target as HTMLInputElement
@@ -25,25 +40,29 @@ export function ProfileFormDialog(props: ProfileFormProps) {
     }
   
     const handleSubmit = async () => {
+      csrftoken = getCookie('csrftoken') as string;
       const response = await fetch(`http://localhost:8000/users/2`, {
+        credentials: 'include',
         method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'X-CSRFToken': csrftoken
+        },
         body: JSON.stringify(profile)
       })
-      if (!response.ok) {
-        setVisible(!visible)}
+      if (response.ok) {
+        setVisible(!visible)
       }
     }
+
 
     return (
       <Dialog open={visible}>
         <DialogTitle>Edit Profile</DialogTitle>
-        <form onSubmit={event => { 
-          event.preventDefault();
-          handleSubmit();
-        }
-        >
+        <form>
         <DialogContent>
-              <Stack direction={"column"} spacing={5}>
+              <Stack direction={"column"} spacing={3}>
                 <TextField id="first_name" label="first name" variant="outlined" value={profile.first_name} onChange={(event) => handleChange(event)} required/>
                 <TextField id="last_name" label="last name" variant="outlined" value={profile.last_name} onChange={(event) => handleChange(event)} required/>
                 <TextField id="title" label="title" variant="outlined" value={profile.title} onChange={(event) => handleChange(event)}/>
@@ -51,7 +70,7 @@ export function ProfileFormDialog(props: ProfileFormProps) {
               </Stack>
         </DialogContent>
         <DialogActions>
-            <Button variant="contained" color="success">Save Profile</Button>
+            <Button variant="contained" color="success" onClick={handleSubmit}>Save Profile</Button>
         </DialogActions>
         </form>
       </Dialog>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 'use client'
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import { Container, Typography, Button, Box} from "@mui/material";
 import {ProfileFormDialog} from './components/profileForm';
 
 export default function Home() {
-  const [profile, setProfile] = useState({first_name: "arnold", last_name: "sanders", title: "super app builder", description: "hood super smart star" })
+  const [profile, setProfile] = useState({first_name: "", last_name: "", title: "", description: ""})
   const [visible, setVisible] = useState(false)
 
+  const fetchUser = async (id: number) => {
+    const user = await fetch(`http://localhost:8000/users/${id}`, {
+      method:"GET",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    })
+    console.log(user)
+  }
+
+  useEffect(()=> {
+    fetchUser(2)
+  }, [])
+  
   return (
     <Container>
     <Typography variant="h1" component="h2">
@@ -21,7 +36,7 @@ export default function Home() {
           <Typography>Address: 2605 Grand Concourse</Typography>
         </Box>
         <Box sx={{justifySelf: "flex-end"}}>
-          <Button variant="contained" color="success" onClick={() => setVisible(!open)}>Edit Profile</Button>
+          <Button variant="contained" color="success" onClick={() => setVisible(!visible)}>Edit Profile</Button>
         </Box>
       </Box>
       <ProfileFormDialog
